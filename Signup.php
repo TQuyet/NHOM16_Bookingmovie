8681dd7f28b3e4e15024dc1a616fa9399b96ec12
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-03-08 15:24:05 +0700

    Fix đăng kí

--- Signup.php
+++ Signup.php
@@ -1,39 +1,41 @@
 <?php include('header.php'); ?>
-
-<body class="text-left form-left">
-  <main class="form-signup">
-    <div class="container">
-      <div class="signup-form">
-        <form action="register_a.php" method="post" enctype="multipart/form-data">
-          <h2>Đăng kí</h2>
-          <p class="hint-text">Tạo tài khoản</p>
-          <div class="container row">
-          <div class="form-group">
-            <div class="row">
-              <div class="col"><input type="text" class="form-control" name="first_name" placeholder="First Name" required="required"></div>
-              <div class="col"><input type="text" class="form-control" name="last_name" placeholder="Last Name" required="required"></div>
-            </div>
+<div class="d-flex justify-content-center">
+  <div class="wrap">
+    <div class="content-top" style="min-height:300px;padding:50px;">
+      <div class="col-md-12 ">
+        <div class="panel panel-default">
+          <div class="mb-3" style="text-align:center; font-size:30px">Đăng kí</div>
+          <div class="panel-body">
+            <?php include('msgbox.php'); ?>
+            <form action="process_signup.php" method="post">
+              <div class="mb-3">
+                Họ và Tên<input name="username" type="text" size="25" placeholder="Họ và Tên" class="form-control" placeholder="Họ và Tên" />
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+              </div>
+              <div class="mb-3">
+                Tài Khoản<input name="email" type="text" size="25" placeholder="Email" class="form-control" placeholder="Email" />
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+              </div>
+              <div class="mb-3">
+                Mật Khẩu
+                <input name="password1" type="password" size="25" placeholder="Password" class="form-control" placeholder="Password" />
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+              </div>
+              <div class="mb-3">
+                Nhập lại Mật Khẩu
+                <input name="password2" type="password" size="25" placeholder="Retype Password" class="form-control" placeholder="Retype Password" />
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+              </div>
+              <div class="form-group 1">
+                <button type="submit" name="btnsignup" class="btn btn-primary">Đăng kí</button>
+              </div>
+              <div>
+                <p class="login-box-msg" style="padding-top:20px">Quay trở lại đăng nhập <a href="login.php">Đăng nhập</a></p>
+              </div>
           </div>
-          <div class="form-group">
-            <input type="email" class="form-control" name="email" placeholder="Email" required="required">
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
-          </div>
-          <div class="form-group">
-            <input type="password" class="form-control" name="cpass" placeholder="Confirm Password" required="required">
-          </div>
-          <div class="form-group">
-            <label class="form-check-label"><input type="checkbox" required="required"> I accept the <a href="#">Terms of Use</a> & <a href="#">Privacy Policy</a></label>
-          </div>
-          <div class="form-group">
-            <button type="submit" name="save" class="btn btn-primary btn-lg btn-block">Đăng kí</button>
-          </div>
-          <div class="text-center">Đã có tài khoản ? <a href="login.php">Đăng nhập</a></div>
-          </div>
-        </form>
-
+        </div>
       </div>
     </div>
-  </main>
-</body>
+  </div>
+</div>
+</div>
