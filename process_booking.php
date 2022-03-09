00562a3eb1055859a782962cf1c7ce48c8ea4cc7
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-03-10 05:48:46 +0700

    Update process_booking.php

--- process_booking.php
+++ process_booking.php
@@ -3,19 +3,27 @@ $conn = mysqli_connect('localhost', 'root', '', 'bookingmovie1');
 if (!$conn) {
     die("Kết nối thất bại");
 }
-if (isset($_POST['datve'])) {
 
-    $sql = "INSERT INTO tbl_booking VALUES ('$idphim','$idrap','$tenphim','$dienvien','$tomtat','$ngaychieu', '$hinhanh')";
+    if (isset($_POST['datve'])) {
+        $screenid = $_POST['screen'];
+        $noseat = $_POST['number'];
+        $giave = $_POST['amount'];
+        $ngaychieu = $_POST['date'];
 
-    $ketqua = mysqli_query($conn, $sql);
+        $sql = "INSERT INTO tbl_bookings ( screen_id, no_seats, amount, ticket_date) VALUES ( '$screenid', '$noseat','$giave', '$ngaychieu')";
 
-    if (!$ketqua) {
-        header("location: error.php"); //Chuyển hướng lỗi
-    } else {
-        echo "them phim thanh cong !"; //Chuyển hướng lại Trang Quản trị
+        $ketqua = mysqli_query($conn, $sql);
+
+        if (!$ketqua) {
+            header("location: error.php"); //Chuyển hướng lỗi
+        } else {
+            echo "Thêm phim thành công !"; //Chuyển hướng lại Trang Quản trị
+            header("location:pagemovie.php");
+        }
+
+
+        // Bước 03: Đóng kết nối
+        mysqli_close($conn);
     }
 
 
-    // Bước 03: Đóng kết nối
-    mysqli_close($conn);
-}
