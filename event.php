2b603600271fe972af4da6dda21a4dcf0eeade9f
06_1951060541_NguyenHuuBach <94445131+NguyenHuuBach1951060541@users.noreply.github.com>
2022-03-19 01:17:23 +0700

    Sự kiện

---
+++ event.php
@@ -0,0 +1,35 @@
+<?php include('header.php'); ?>
+
+<head>
+    <link rel="stylesheet" href="event.css">
+</head>
+
+<body>
+    <h1 style="color: red;padding:35px;text-decoration-line: underline;" class="text-center">ƯU ĐÃI</h1>
+    <div class="row">
+        <div class="column">
+            <img style="width: 18rem;" src="https://vnpay.vn/s1/statics.vnpay.vn/2022/3/0r3ze6slzk11646637821937.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://cdn.scb.com.vn/picture/1000x1000_fb_0_cgv_.webp" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://stc.shopiness.vn/deal/2017/09/07/7/0/4/7/1504781721390_540.png" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://www.tiendauroi.com/wp-content/uploads/2020/02/zalopay-mua-2-tinh-tien-1-khi-mua-ve-xem-phim-tai-bhd-qua-zalopay.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://i.imgur.com/JQh3OkQ.jpeg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://lichchieuphim.net/storage/promotions/dat-ve-beta-cinemas-hoan-tien-10-khi-thanh-toan-qua-vi-airpay/thumbnail.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://vnpay.vn/s1/statics.vnpay.vn/2022/3/0r3ze6slzk11646637821937.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <div class="column">
+            <img style="width: 18rem;" src="https://vnpay.vn/s1/statics.vnpay.vn/2022/3/0r3ze6slzk11646637821937.jpg" class="img-thumbnail" alt="...">
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
--- header.php
+++ header.php
@@ -28,7 +28,7 @@ session_start();
                     <a class="nav-link" aria-current="page" href="pagemovie.php">Phim</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Ưu đãi </a>
+                    <a class="nav-link" href="event.php">Ưu đãi </a>
                 </li>
             </ul>
         </div>
