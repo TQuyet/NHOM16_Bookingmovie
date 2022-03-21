0df77dc530e569f1ff9e30aba57b3fe235933738
Nguyen Trong Quyet <1951060972@e.tlu.edu.vn>
2022-03-21 17:58:54 +0700

    111

--- adminn.php
+++ adminn.php
@@ -55,6 +55,29 @@ session_start();
                 </p>
                 <a class="btn btn-primary" href="update_movie.php?id=<?php echo $m['movie_id']; ?>">Sửa</a>
                 <a id="delete" class="btn btn-danger" href="process_delete_movie.php?id=<?php echo $m['movie_id']; ?>">Xóa phim</a>
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#deletemodal">
+                    Xóa Phim1
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="deletemodal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Cảnh báo !!!</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                Phim đã xóa không thể khôi phục !!!
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+                                <button type="button" id="delete" name="delete" class="btn btn-danger">Vẫn xóa</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -65,9 +88,6 @@ session_start();
 
     <body>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-            $('#delete').click(function() {
-                alert('Phim đã xóa!');
-            });
         </script>
     </body>
 
